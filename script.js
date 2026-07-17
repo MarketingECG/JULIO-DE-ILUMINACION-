e2db8109995b9130b3904a2bdbf07f4f2fa8c57c
MarketingECG <Marketing@ecgcol.com>
2026-07-17 12:22:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSt9KORPY7MxVuP8WgtUsy290ueFK0twcckpJGvuV540cf25bwBq2k0yJsr_oeAs4N79D-yZDbvaAPY/pub?gid=0&single=true&output=csv";
